65e6d4241a8067972373517032e585bd732c822a
仲松　拓哉 <takuya.nakamatsu@PC-3385.local>
2022-06-26 19:21:44 +0900

    importの書き方をalias指定へ変更

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,20 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...{
+        '@': path.join(__dirname),
+        '@styles': path.join(__dirname, 'styles/pages'),
+        '@components': path.join(__dirname, 'components'),
+      },
+    };
+    return config;
+  },
+
 }
 
 module.exports = nextConfig
